a3483b9164bd26cd365d8a2e25cd134609ed2845
lingehenshuaiqi <113441802+lingehenshuaiqi@users.noreply.github.com>
2022-09-16 00:12:27 +0800

    Add files via upload
    姓名：黄钊霖
    学号2020144112
    大卫星

---
+++ first.py
@@ -0,0 +1,12 @@
+import turtle
+from turtle import*
+pendown()
+for j in range(6):
+    forward(100)
+    right(120)
+    forward(100)
+    left(60)
+for j in range(6):
+    right(60)
+    forward(100)
+done()
